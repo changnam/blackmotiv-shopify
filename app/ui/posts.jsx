44e3dff7fe37c82f7291ecdd6d5988ae64471ca4
Changnam Go <changnam.go@gmail.com>
2025-02-16 10:44:04 +0900

    server component 에서 await 없이 data fetch 하여 Promise 객체를 client component에 넘기는 방법. Suspense 사용하여 fallback 을 먼저 보여줌. client 에서는 use hook 을 사용해야함

---
+++ app/ui/posts.jsx
@@ -0,0 +1,16 @@
+'use client';
+import { use } from 'react';
+ 
+export default function Posts({
+  posts,
+}) {
+  const allPosts = use(posts)
+ 
+  return (
+    <ul>
+      {allPosts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  )
+}
