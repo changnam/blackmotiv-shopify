901e34c4cf8a6145d7f048ed0255a0cd65fa9ebf
Changnam Go <changnam.go@gmail.com>
2025-02-16 11:45:06 +0900

    loading.jsx 를 사용하여 server component data fetch 시간동안 suspense 적용

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,16 +1,11 @@
-import Posts from "@/app/ui/posts";
-import {delay} from "@/app/lib/utils"
-import { Suspense } from 'react'
-
 export default async function Page() {
-  // const data = await fetch('https://api.vercel.app/blog',{cache: "no-store"})
-  // const posts = await data.json()
-  // Using the function:
-  const posts =  fetch('https://api.vercel.app/blog',{cache: "no-store"}).then(res => res.json);
-
+  const data = await fetch('https://api.vercel.app/blog',{cache: "no-store"})
+  const posts = await data.json()
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Posts posts={posts} />
-    </Suspense>
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
   )
 }
