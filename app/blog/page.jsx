1b05f440bbf6d1551bc51dcd8f8bc05686fd6e0f
Changnam Go <changnam.go@gmail.com>
2025-02-16 11:41:41 +0900

    refactoring code. data fetch 와 suspense 사용

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -2,18 +2,11 @@ import Posts from "@/app/ui/posts";
 import {delay} from "@/app/lib/utils"
 import { Suspense } from 'react'
 
-async function fetchData(url) {
-  await delay(3000);
-  return fetch(url) // Fetch data from the API
-    .then(response => response.json()); // Convert response to JSON
-}
-
-
 export default async function Page() {
   // const data = await fetch('https://api.vercel.app/blog',{cache: "no-store"})
   // const posts = await data.json()
   // Using the function:
-  const posts =  fetchData('https://api.vercel.app/blog',{cache: "no-store"});
+  const posts =  fetch('https://api.vercel.app/blog',{cache: "no-store"}).then(res => res.json);
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
