297fd14860000c4a892ef36d9413ef051f12448a
Changnam Go <changnam.go@gmail.com>
2025-02-16 21:07:03 +0900

    tailwind 설정 완료, mjs 에서 js 로 변경

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,11 +1,14 @@
+// import {ShopingCart} from "@heroicons/react/24/outline";
+
 export default function Navbar(){
     return (
         <nav className="flex  ">
-            <button className="flex-none">햄버거아이콘</button>
-                <ul className="flex-auto flex flex-row gap-x-4 px-4">
-                    <li className="flex-none ml-auto">Home</li>
-                    <li className="flex-none">About</li>
-                </ul>                
+            <button className="flex-none md:hidden">햄버거아이콘</button>
+            <ul className="flex-auto flex flex-row gap-x-4 px-4">
+                <li className="flex-none ml-auto">Home</li>
+                <li className="flex-none">About</li>
+            </ul>       
+            {/*<button className="flex-none "><ShopingCart /></button>*/}         
         </nav> 
     )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+const plugin= require("tailwindcss/plugin");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+      },
+      blink: {
+        "0%": { opacity: 0.2 },
+        "20%": { opacity: 1 },
+        "100%": { opacity: 0.2 },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.3s ease-in-out",
+        blink: "blink 1.4s both infinite"
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+    plugin(({ matchUtilities, theme }) => {
+      matchUtilities(
+        {
+          "animation-delay": (value) => {
+            return {
+              "animation-delay": value,
+            };
+          },
+        },
+        {
+          values: theme("transitionDelay"),
+        }
+      );
+    }),
+  ],
+};
