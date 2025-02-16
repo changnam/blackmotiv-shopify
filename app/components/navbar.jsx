ea7f3d1a650b1dcd3df6dc027a50ffd516cc43bc
Changnam Go <changnam.go@gmail.com>
2025-02-16 17:33:58 +0900

    flex 에서 오른쪽으로 정렬 첫번째 item에 ml 적용

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,5 +1,11 @@
 export default function Navbar(){
     return (
-        <div className="text-center">navbar </div>
+        <nav className="flex container ">
+            <button className="flex-none">햄버거아이콘</button>
+                <ul className="flex-auto flex flex-row gap-x-4 px-4">
+                    <li className="flex-none ml-auto">Home</li>
+                    <li className="flex-none">About</li>
+                </ul>                
+        </nav> 
     )
 }
